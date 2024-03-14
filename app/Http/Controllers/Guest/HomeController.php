6bf943c9e3bc5c43eec32ef19ff7b82be3c91e9e
omidomw <omidomw@gmail.com>
2024-03-14 11:03:34 +0330

    redis cache user counts

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\BankAccount;
+use App\Models\User;
+use Error;
+use Exception;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        return view('guest.welcome');
+        $usersCount = Cache::get('homepage-users-count');
+        if(!$usersCount) {
+            $usersCount = User::count();
+            Cache::set('homepage-users-count', $usersCount, 10);
+        }
+
+        $bankAccountsCount = BankAccount::count();
+
+        return view('guest.welcome', compact('usersCount', 'bankAccountsCount'));
     }
 }
